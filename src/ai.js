2243ed8feba5535bce60176738bfff3949eb0d06
r-a-q <156955463+r-a-q@users.noreply.github.com>
2026-04-23 00:21:02 -0700

    updated Claude API alt text instructions

--- src/ai.js
+++ src/ai.js
@@ -1,12 +1,5 @@
 import Anthropic from "@anthropic-ai/sdk"
 
-const SYSTEM_PROMPT =
-    `You will be given a url for a painting. You will generate alternative text of the image. Follow the best practices: 
-    1) Keep it short, usually 1-2 sentences. Don’t overthink it
-    2) Consider key elements of why you chose this image, instead of describing every little detail.
-    3) No need to say “image of” or “picture of.”
-    4) End the alt text sentence with a period.`
-
 const anthropic = new Anthropic({
     apiKey: import.meta.env.VITE_ANTHROPIC_API_KEY,
     dangerouslyAllowBrowser: true,
@@ -17,7 +10,6 @@ export async function getAltTextFromClaude(base64Data) {
     const msg = await anthropic.messages.create({
         model: "claude-haiku-4-5-20251001",
         max_tokens: 1024,
-        system: SYSTEM_PROMPT,
         messages: [
             {
                 role: "user",
@@ -32,12 +24,15 @@ export async function getAltTextFromClaude(base64Data) {
                     },
                     {
                         type: "text",
-                        text: "You will be given a url for a painting. You will generate alternative text of the image. Include the painting style in the description" +
+                        text: "You will be given a url for a painting. You will generate alternative text of the image relevant for screen readers. " +
+                            "Include the painting style in the description" +
                             "Follow the best practices:" +
                             "1) Keep it short, usually 1-2 sentences. Don’t overthink it" +
                             "2) Consider key elements of why you chose this image, instead of describing every little detail." +
                             "3) No need to say “image of” or “picture of.”" +
-                            "4) End the alt text sentence with a period."
+                            "4) End the alt text sentence with a period." +
+                            "Do not interpret the painting, just describe what the painting looks like and point out key elements necessary." +
+                            "Provide ONLY the description itself. Do not include headers, titles, or labels like '# Alt Text'."
                         //improve prompt for more accurate/relevant information for alternative text
                     }
                 ]
