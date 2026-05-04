7901f666acc87be4153a2d0e1020b1994fd2c6c6
r-a-q <156955463+r-a-q@users.noreply.github.com>
2026-05-04 08:17:50 -0700

    reformat and add comments to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import Museum from './Museum.jsx'
 
+//place our museum in the root
 createRoot(document.getElementById('root')).render(
-    <Museum />
+    <Museum/>
 )
