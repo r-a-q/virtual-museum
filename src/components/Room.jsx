b916ce6f6f55555f7fda98611981ab6d1e0e4f13
r-a-q <156955463+r-a-q@users.noreply.github.com>
2026-05-04 07:59:58 -0700

    reformat and add comments to Room.jsx

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,14 +1,22 @@
-import { useTexture } from '@react-three/drei'
+import {useTexture} from '@react-three/drei'
 import * as THREE from 'three'
 
-
+/**
+ * walls, roof, and ceiling of the virtual museum
+ * @returns {React.JSX.Element} - walls, roof, and ceiling of the virtual museum
+ * @constructor
+ */
 export default function Room() {
 
+    //map texture of the walls to jpg image
     const wall = useTexture("/navy_wall.jpg")
     return (
         <>
+            {/*set position of the room in space*/}
             <mesh position={[0, 1.5, 0]}>
+                {/*set the dimensions of the room*/}
                 <boxGeometry args={[10, 7, 20]}/>
+                {/*set the walls and ceiling texture*/}
                 <meshStandardMaterial map={wall} side={THREE.BackSide}/>
             </mesh>
         </>
